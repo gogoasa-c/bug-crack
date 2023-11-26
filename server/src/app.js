649d1183e48d0c5da085d3a4b5b331be1a15df75
Brescar <ghitavalentin2001@gmail.com>
2023-11-26 15:21:27 +0200

    Added new route for post a user ;; verified functionality in postman and db

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const {SERVER_PORT} = require("./constant/constant.js");
 const router = require("./route/api.js");
 const sequelize = require("./db/db.js");
 const Bug = require("./model/bug.js");
+const User = require("./model/user.js");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
--- server/src/route/api.js
+++ server/src/route/api.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
 const BugController = require("../controller/BugController.js");
+const UserController = require("../controller/UserController.js");
 
 
 router.post("/bug", BugController.add);
+
+router.post("/user", UserController.add);
 
 
 module.exports = router;
